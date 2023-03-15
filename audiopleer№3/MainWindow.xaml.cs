a66d385d1fa4ce14e64e703f8c412a67d4299f8c
ulyanaukolova <isip_u.a.ukolova@mpt.ru>
2023-03-15 09:35:35 +0300

    без ремоте адд ориджин

--- audiopleer№3/MainWindow.xaml.cs
+++ audiopleer№3/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,15 +24,15 @@ namespace audiopleer_3
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        private static List<string> list = new List<string>();
+        private List<string> peremesh = new List<string>();    
+        int pereklch = 0;
+        int stop_play = 0;
         public MainWindow()
         {
             InitializeComponent();
-            media.Source = new Uri("C:\\Users\\MASTER\\audio\\pharaoh-odnim-celym-mp3");
-            media.Play();
-
-            media.Volume = 0.7;
         }
+
         private void AudioSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             media.Position = new TimeSpan(Convert.ToInt64(audioSlider.Value));
@@ -49,25 +50,67 @@ namespace audiopleer_3
             if ( result == CommonFileDialogResult.Ok)
             {
                 string[] files = Directory.GetFiles(dialog.FileName);
-                foreach ( string file in files )
+                Regex regex = new Regex(@"\w*\.mp3");
+                foreach (string item in files)
                 {
-                    FileTxt.Text = file + "\n";
+                    if (regex.IsMatch(item))
+                    {
+                        Lsongs.Items.Add(regex.Match(item).Value);
+                        list.Add(item);
+
+                    }
                 }
+
             }
-            ofd.Filter = "MP3 files (*.mp3)|*.mp3";
+            media.Source = new Uri(list[0]);
+            media.Play();
 
         }
 
         private void Nazad_Click(object sender, RoutedEventArgs e)
         {
-            var a = new Uri(System.IO.Path.GetFullPath(@"..\..\..\5094_900.png"), UriKind.Relative);
-            ImageBrush nolik = new ImageBrush(new BitmapImage(a));
-           
+            pereklch--;
+            media.Source = new Uri(list[pereklch]);
+            media.Play();
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
 
+        private void Vpered_Click(object sender, RoutedEventArgs e)
+        {
+            pereklch++;
+            media.Source = new Uri(list[pereklch]);
+            media.Play();
+        }
+
+        private void Ugrat_Click(object sender, RoutedEventArgs e)
+        {
+            media.Play();
+        }
+
+        private void Povtor_Click(object sender, RoutedEventArgs e)
+        {
+            media.Stop();
+            media.Play();
+        }
+
+        private void Change_Click(object sender, RoutedEventArgs e)
+        {
+            peremesh = list;
+            Random R = new Random();
+            
+            for (int i = 0; i < peremesh.Count; i++)
+            {
+                string tmp = peremesh[0];
+                peremesh.RemoveAt(0);
+                peremesh.Insert(R.Next(peremesh.Count), tmp);
+            }
+            media.Source = new Uri(peremesh[0]);
+            media.Play();
+        }
+
+        private void STOP_Click(object sender, RoutedEventArgs e)
+        {
+            media.Stop();
         }
     }    
 }
